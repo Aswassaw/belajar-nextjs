ccbc00b9c6638a99aef1c1ac8a52a7c70e064598
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-24 02:47:04 +0700

    Navigating Programmatically

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,24 @@
+import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import React from "react";
 
 const Home = () => {
+  const router = useRouter();
+
+  // Redirect
+  const handleClick = () => {
+    setTimeout(() => {
+      router.push("/product");
+    }, 3000);
+  };
+
+  // Redirect with replace
+  const handleReplace = () => {
+    setTimeout(() => {
+      router.replace("/product");
+    }, 3000);
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
@@ -13,6 +30,9 @@ const Home = () => {
       <Link href='/product'>
         <a>Product</a>
       </Link>
+      <br />
+      <button onClick={handleClick}>Place Order</button>
+      <button onClick={handleReplace}>Place Order Replace</button>
     </div>
   );
 };
