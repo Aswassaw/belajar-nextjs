1ad8e3dcf073781cb4b72d2f328641e10c679b7a
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-27 02:05:24 +0700

    Global Styles and Use Bootstrap

---
+++ pages/styling.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Styling = () => {
+  return (
+    <div>
+      <h1>Styling</h1>
+      <button type='button' className='btn btn-primary'>
+        Button
+      </button>
+    </div>
+  );
+};
+
+export default Styling;
