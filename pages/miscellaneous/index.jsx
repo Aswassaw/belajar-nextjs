b4b8d1cc7e488a46dbeb63975a2add3a93a4d691
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-28 00:38:14 +0700

    Head Component

--- pages/miscellaneous/index.jsx
+++ pages/miscellaneous/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Head from "next/head";
 import Footer from "../../components/miscellaneous/Footer";
 
 const Miscellaneous = () => {
   return (
     <div>
+      <Head>
+        <title>Humanism</title>
+        <meta name="description" content="Belajar NextJS" />
+      </Head>
       <h1>Manusia Besi</h1>
     </div>
   );
