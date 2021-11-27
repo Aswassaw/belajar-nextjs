704890b9ae86fe0e0d251985022fd3040f98d193
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-28 01:58:14 +0700

    next export untuk menghasilkan static html

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import "../styles/globals.css";
-import "../styles/layout.css";
+import "/styles/globals.css";
+import "/styles/layout.css";
 import Head from "next/head";
-import Header from "../components/miscellaneous/Header";
-import Footer from "../components/miscellaneous/Footer";
+import Header from "/components/miscellaneous/Header";
+import Footer from "/components/miscellaneous/Footer";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
@@ -11,24 +11,28 @@ const theme = {
   },
 };
 
+const HeadRoot = () => (
+  <Head>
+    <link
+      href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
+      rel='stylesheet'
+      integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC'
+      crossorigin='anonymous'
+    />
+    <script
+      src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+      integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM'
+      crossorigin='anonymous'
+    ></script>
+  </Head>
+);
+
 function MyApp({ Component, pageProps }) {
   // Jika di suatu component terdapat fungsi bernama getLayout
   if (Component.getLayout) {
     return Component.getLayout(
       <>
-        <Head>
-          <link
-            href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
-            rel='stylesheet'
-            integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC'
-            crossorigin='anonymous'
-          />
-          <script
-            src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
-            integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM'
-            crossorigin='anonymous'
-          ></script>
-        </Head>
+        <HeadRoot />
         <ThemeProvider theme={theme}>
           <Component {...pageProps} />
         </ThemeProvider>
@@ -38,19 +42,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <link
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
-          rel='stylesheet'
-          integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC'
-          crossorigin='anonymous'
-        />
-        <script
-          src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
-          integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM'
-          crossorigin='anonymous'
-        ></script>
-      </Head>
+      <HeadRoot />
       <Header />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
